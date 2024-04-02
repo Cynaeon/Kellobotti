7a5afd64c251443db9ec1f10a396b980b28623e2
Joni Tasala <jonitasala@gmail.com>
2024-04-02 22:34:30 +0300

    Make 00:00 invalid bonus time

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,7 @@ import { KelloTime, GET_TIMES } from "./globals";
 
 /** Valid random kello is not included in the official kello times, and is also further than a minute away from one. */
 export function isValidRandomKello(kello: KelloTime): boolean {
+    if (kello.hour === 0 && kello.minute === 0) { return false; }
     return GET_TIMES.every(
         time => time.hour !== kello.hour
         || Math.abs(time.minute - kello.minute) > 1
